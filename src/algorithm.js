ed73576aff2b158f0625cdba1a97578a498bbf46
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2018-05-09 20:42:15 -0500

    Scraping method returns object instead of just rating

--- src/algorithm.js
+++ src/algorithm.js
@@ -16,7 +16,7 @@ module.exports = courseNumber => {
       }
     });
     
-    var rts = courseNumber.split(' ');
+    let rts = courseNumber.split(' ');
     db.serialize(() => {
       // SQL query for getting professors and their grade data for a course
       let sql = `SELECT prof AS name, a1, a2, a3, b1, b2, b3, c1, c2, c3, d1, d2, d3, f
@@ -24,45 +24,47 @@ module.exports = courseNumber => {
                 WHERE dept = ? AND course_nbr = ?
                 ORDER BY name`;
 
-      var profs = [];
+      let profs = [];
       // 2D array storing percentage of class that got each grade for each prof
-      var percentages = [];
+      let percentages = [];
       // Gets information on each professor teaching the given course
       db.each(sql, rts, (err, row) => {
         if (err) {
           throw err;
         }
         // Reorders full name into first name, last name
-        var splitName = row.name.split(/ +/);
-        var finalName = splitName[1] + ' ' + splitName[0];
+        let splitName = row.name.split(/ +/);
+        let finalName = splitName[1] + ' ' + splitName[0];
         profs.push(finalName);
         delete row.name;
         // Stores info of the percentage of each class that got each grade
-        var totalStu = Object.values(row).reduce((a, b) => a + b, 0);
-        var currPerc = [];
+        let totalStu = Object.values(row).reduce((a, b) => a + b, 0);
+        let currPerc = [];
         Object.values(row).forEach(numStu => currPerc.push(numStu / totalStu));
         percentages.push(currPerc);
       }, () => {
-        var promises = profs.map(prof => {
-          return getProfRating(prof);
+        let promises = profs.map(prof => {
+          return getProfInfo(prof);
         });
-        // Gets the Rate My Professors rating for each professor
+        // Gets the Rate My Professors info for each professor
+        // Calculates the Pick Your Prof score for each professor
         Promise.all(promises)
-          .then(ratings => {
-            var scores = getScores(profs, ratings, percentages);
+          .then(responses => {
+            let scores = getScores(profs, responses, percentages);
             // Zip up the profs and scores into an object
-            var profData = {};
+            let profData = {};
             profs.forEach((prof, i) => profData[prof] = scores[i]);
             console.log(profData);
             // Sort professors by their scores in descending order
-            var sortedProfs = Object.keys(profData).sort((a, b) => {
+            let sortedProfs = Object.keys(profData).sort((a, b) => {
               return profData[b] - profData[a];
             });
 
             // Wrap sorted professor list and score data into an object
-            var finalData = {};
-            finalData.list = sortedProfs;
-            finalData.scores = profData;
+            let finalData = {
+              list: sortedProfs,
+              scores: profData
+            };
 
             // FINAL OUTPUT
             resolve(finalData);
@@ -76,21 +78,25 @@ module.exports = courseNumber => {
 }
 
 // Gets the PickYourProf score for each professor
-function getScores(profs, ratings, percentages) {
+function getScores(profs, responses, percentages) {
+  // Gets professor ratings from response objects
+  let ratings = [];
+  responses.forEach(res => ratings.push(res.rating));
+
   // Gets average rating of professors that have ratings on Rate My Professors
-  var numProfs = 0;
-  var sum = 0.0;
+  let numProfs = 0;
+  let sum = 0.0;
   ratings.forEach(rating => {
     // Only adds to sum if professor has a rating
-    if (rating !== '-1.0') {
+    if (rating) {
       sum += parseFloat(rating);
       numProfs++;
     }
   });
-  var avg = sum / numProfs;
+  let avg = sum / numProfs;
 
-  var scores = [];
-  for (var i = 0; i < profs.length; i++) {
+  let scores = [];
+  for (let i = 0; i < profs.length; i++) {
     // Sets Rate My Professor rating to average if professor has no rating
     if (ratings[i] === '-1.0') {
       ratings[i] = avg;
@@ -102,7 +108,7 @@ function getScores(profs, ratings, percentages) {
     // GPA that a student gets for getting between an A+ and an F
     const gpa = [4, 4, 3.67, 3.33, 3, 2.67, 2.33, 2, 1.67, 1.33, 1, 0.67, 0];
     // Calculates average GPA of students in the course
-    var avgGpa = 0;
+    let avgGpa = 0;
     percentages[i].forEach((percent, j) => avgGpa += percent * gpa[j]);
     // Normalize average GPA to be out of 5 instead of 4
     avgGpa *= (5.0 / 4.0);
@@ -110,7 +116,7 @@ function getScores(profs, ratings, percentages) {
     // Score is normalized average GPA + Rate My Professors rating
     // If none of the professors are on RMP, score is avg GPA scaled out of 10
     // Max score is 10
-    var score = (numProfs === 0) ? avgGpa + ratings[i] : avgGpa * 2;
+    let score = (numProfs === 0) ? avgGpa + ratings[i] : avgGpa * 2;
     scores.push(score);
   }
   return scores;
@@ -121,17 +127,17 @@ const baseURL = 'http://www.ratemyprofessors.com/search.jsp?queryoption=HEADER' 
   '&queryBy=teacherName&schoolName=University+of+Texas+at+Austin&schoolID=1255&query=';
 
 // Gets the rating for a professor
-function getProfRating(name) {
+function getProfInfo(name) {
   return new Promise((resolve, reject) => {
     // Goes through list of professors
     name = name.replace(' ', '+');
     // Link to query Rate My Professors
-    var link = baseURL + name;
+    let link = baseURL + name;
     // Searches for the professor on Rate My Professors
     // If professor found, then gets the professor's rating
-    fetchProfLink(link)
+    scrapeProfLink(link)
       .then(endLink => {
-        fetchProfRating('http://www.ratemyprofessors.com' + endLink)
+        scrapeProfInfo('http://www.ratemyprofessors.com' + endLink)
          .then(rating => resolve(rating))
          .catch(e => resolve(e));
       })
@@ -139,47 +145,53 @@ function getProfRating(name) {
   });
 }
 
-// Gets link of professor on Rate My Professors
-function fetchProfLink(link) {
+// Scrapes link of professor on Rate My Professors
+function scrapeProfLink(link) {
   return new Promise((resolve, reject) => {
     fetch(link)
       .then(res => res.text())
       .then(body => {
-        var $ = Cheerio.load(body);
+        let $ = Cheerio.load(body);
         // Checks if any results are found
-        var htmas = $('div.not-found-box').next().html();
+        let htmas = $('div.not-found-box').next().html();
         if (!htmas.includes('Your search')) {
           // Gets the link for the first result
           $('.listings').filter(function() {
-            var endLink = $($(this)[0].children[3].children[1]).attr('href');
+            let endLink = $($(this)[0].children[3].children[1]).attr('href');
             resolve(endLink);
           });
         } else {
           // No results found in Rate My Professors for the professor
-          reject('-1.0');
+          reject({});
         }
       })
-      .catch(err => reject('-1.0'));
+      .catch(err => reject({}));
   });
 }
 
-// Gets rating of professor on Rate My Professors
-function fetchProfRating(link) {
+// Possible keys in the response object when getting professor info
+const responseKeys = ['rating'];
+
+// Scrapes info of professor on Rate My Professors
+function scrapeProfInfo(link) {
   return new Promise((resolve, reject) => {
     fetch(link)
       .then(resp => resp.text())
       .then(html => {
         // Queries the link to the professor's page
-        var $ = Cheerio.load(html);
-        var overallQuality = $('[class=\'breakdown-container quality\'] .grade').html();
-        if (overallQuality != null) {
-          overallQuality = overallQuality.replace(/\s+/g, '');
+        let $ = Cheerio.load(html);
+        let response = {};
+        let rating = $('[class=\'breakdown-container quality\'] .grade').html();
+        if (rating) {
+          rating = rating.replace(/\s+/g, '');
           // Overall rating found for professor
-          resolve(overallQuality);
-        } else {
-          // No overall rating found for professor
-          reject('-1.0');
+          response.rating = rating;
         }
+        // Reject if response is empty
+        if (response == {}) {
+          reject(response);
+        }
+        resolve(response);
       });
   });
 }
--- src/server.js
+++ src/server.js
@@ -7,14 +7,14 @@ const multer = require('multer');
 const app = express();
 const upload = multer();
 
-var PORT = process.env.PORT || 3000;
+let PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.post('/home', upload.any(), (req, res) => {
-  var course = req.body.course;
+  let course = req.body.course;
   console.log('Course:', course);
 
   // Get the sorted list of professors and their scores
@@ -26,11 +26,11 @@ app.post('/home', upload.any(), (req, res) => {
         // HTML tags for output formatting
         const leftTag = '<td style="font-size:18px;font-family:Roboto;color:black;">';
         const rightTag = '<td style="font-size:18px;font-family:Roboto;color:black;">';
-        var output = '<table style="width:40%;margin-left:26%;margin-right:20%;">';
+        let output = '<table style="width:40%;margin-left:26%;margin-right:20%;">';
         output += `<tr>${leftTag}<b>Professor</b></td>${rightTag}<b>Score</b></td></tr>`;
         data.list.forEach(prof => {
           // Formats the name correctly
-          var name = formatName(prof);
+          let name = formatName(prof);
           output += '<tr>';
           output += (leftTag + name + '</td>');
           output += (rightTag + (Math.round(data.scores[prof] * 100) / 100) + '</td>');
@@ -54,9 +54,9 @@ app.listen(PORT, () => {
 
 function formatName(name) {
   // Capitalize only the first letter of first and last name
-  var arr = name.split(' ');
-  var first = arr[0].substring(0, 1) + arr[0].substring(1).toLowerCase();
-  var last = (arr[1].substring(0, 1) + arr[1].substring(1).toLowerCase());
+  let arr = name.split(' ');
+  let first = arr[0].substring(0, 1) + arr[0].substring(1).toLowerCase();
+  let last = (arr[1].substring(0, 1) + arr[1].substring(1).toLowerCase());
   // Fix the cases where there is a hyphen in the person's name
   arr = first.split('-');
   if (arr.length > 1)
