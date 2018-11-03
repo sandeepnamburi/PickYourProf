aab46cd3e2b47278323b3fc4b477b5dad287186c
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2018-11-02 23:37:59 -0500

    Implemented SQLite caching of RMP data

--- src/algorithm.js
+++ src/algorithm.js
@@ -7,55 +7,69 @@ const Cheerio = require('cheerio');
 const sqlite3 = require('sqlite3').verbose();
 const Sentiment = require('sentiment');
 const sentiment = new Sentiment();
+const moment = require('moment');
+
+// SQL query for getting professors and their grade data for a course
+let PROF_QUERY = `SELECT prof AS name, a1, a2, a3, b1, b2, b3, c1, c2, c3, d1, d2,
+                  d3, f, rating, difficulty, take_again, sentiment, last_updated
+                  FROM agg
+                  INNER JOIN rmp_cache
+                  ON agg.prof=rmp_cache.professor
+                  WHERE dept = ? AND course_nbr = ?
+                  ORDER BY name`;
+
+// SQL query for caching new RMP data
+let CACHE_QUERY = `UPDATE rmp_cache
+                  SET rating = ?, difficulty = ?, take_again = ?, sentiment = ?, last_updated = ?
+                  WHERE professor = ?;`
 
 module.exports = courseNumber => {
   return new Promise((resolve, reject) => {
-    let db = new sqlite3.Database('./grades.db', sqlite3.OPEN_READONLY, err => {
+    let db = new sqlite3.Database('./grades.db', sqlite3.OPEN_READWRITE, err => {
       if (err) {
         console.error(err.message);
       } else {
         console.log('Connected to the grades database.');
       }
     });
-    
+
     let rts = courseNumber.split(' ');
     db.serialize(() => {
-      // SQL query for getting professors and their grade data for a course
-      let sql = `SELECT prof AS name, a1, a2, a3, b1, b2, b3, c1, c2, c3, d1, d2, d3, f, rating, difficulty, take_again, sentiment, last_updated
-                FROM agg
-                INNER JOIN rmp_cache
-                ON agg.prof=rmp_cache.professor
-                WHERE dept = ? AND course_nbr = ?
-                ORDER BY name`;
-
+      // List of professor objects
       let profs = [];
-      // 2D array storing percentage of class that got each grade for each prof
-      let percentages = [];
       // Gets information on each professor teaching the given course
-      db.each(sql, rts, (err, row) => {
+      db.each(PROF_QUERY, rts, (err, row) => {
         if (err) {
           throw err;
         }
         // Reorders full name into first name, last name
         let splitName = row.name.split(/,?\s+/);
         let finalName = splitName[1] + ' ' + splitName[0];
-        profs.push(finalName);
-        delete row.name;
-        // Stores info of the percentage of each class that got each grade
-        let totalStu = Object.values(row).reduce((a, b) => a + b, 0);
-        let currPerc = [];
-        Object.values(row).forEach(numStu => currPerc.push(numStu / totalStu));
-        percentages.push(currPerc);
+        row.oldName = row.name;
+        row.name = finalName;
+        let currPerc = getGradePercentages(row.a1, row.a2, row.a3, row.b1, row.b2,
+          row.b3, row.c1, row.c2, row.c3, row.d1, row.d2, row.d3, row.f);
+        row.currPerc = currPerc;
+        row.dirty = false;
+        profs.push(row);
       }, () => {
         let promises = profs.map(prof => getProfInfo(prof));
         // Gets the Rate My Professors info for each professor
         // Calculates the Pick Your Prof score for each professor
         Promise.all(promises)
           .then(responses => {
-            let scores = getScores(profs, responses, percentages);
+            profs.forEach((prof, i) => {
+              let resp = responses[i];
+              // Cache new RMP data to database if prof object is dirty
+              if (prof.dirty) {
+                db.run(CACHE_QUERY, resp.rating, resp.difficulty, resp.again, resp.sentimentAvg, prof.last_updated, prof.oldName);
+              }
+            });
+
+            let scores = getScores(profs, responses);
             // Zip up the profs and scores into an object
             let profData = {};
-            profs.forEach((prof, i) => profData[prof] = scores[i]);
+            profs.forEach((prof, i) => profData[prof.name] = scores[i]);
             console.log(profData);
             // Sort professors by their scores in descending order
             let sortedProfs = Object.keys(profData).sort((a, b) => {
@@ -67,31 +81,34 @@ module.exports = courseNumber => {
               list: sortedProfs,
               scores: profData
             };
-
+            db.close();
             // FINAL OUTPUT
             resolve(finalData);
           });
       });
-      
-      db.close();
     });
-
   });
 }
 
+// Stores info of the percentage of each class that got each grade
+function getGradePercentages(...distribution) {
+  let totalStu = distribution.reduce((a, b) => a + b, 0);
+  distribution.forEach((numStu, i) => distribution[i] = numStu / totalStu);
+  return distribution;
+}
+
 // Gets the PickYourProf score for each professor
-function getScores(profs, responses, percentages) {
+function getScores(profs, responses) {
   // Gets the average of all the professor's metric averages
   let numProfs = 0;
   let sum = 0;
   let metricAverages = [];
   let metricAverage;
   for (let resp of responses) {
-    if (Object.keys(resp).length === 0) {
+    if (!resp.rating && !resp.difficulty && !resp.again && !resp.sentimentAvg) {
       metricAverages.push(-1);
     } else {
       numProfs++;
-      normalizeMetrics(resp);
       metricAverage = getMetricAverage(resp);
       metricAverages.push(metricAverage);
       sum += metricAverage;
@@ -110,7 +127,7 @@ function getScores(profs, responses, percentages) {
     const gpa = [4, 4, 3.67, 3.33, 3, 2.67, 2.33, 2, 1.67, 1.33, 1, 0.67, 0];
     // Calculates average GPA of students in the course
     avgGpa = 0;
-    percentages[i].forEach((percent, j) => avgGpa += percent * gpa[j]);
+    profs[i].currPerc.forEach((percent, j) => avgGpa += percent * gpa[j]);
     // Normalize average GPA to be out of 5 instead of 4
     avgGpa *= 5 / 4;
 
@@ -140,6 +157,44 @@ function getScores(profs, responses, percentages) {
     }
     return metricSum / numMetrics;
   }
+}
+
+// Base URL for HTTP request
+const baseURL = 'http://www.ratemyprofessors.com/search.jsp?queryoption=HEADER' +
+  '&queryBy=teacherName&schoolName=University+of+Texas+at+Austin&schoolID=1255&query=';
+
+// Gets the rating for a professor
+function getProfInfo(prof) {
+  return new Promise((resolve, reject) => {
+    // If the cache entry is empty or old
+    if (!prof.last_updated || moment().diff(moment(prof.last_updated), 'days') > 7) {
+      // Set dirty to true if need to scrape RateMyProfessors for new data
+      prof.dirty = true;
+      prof.last_updated = moment().valueOf();
+      let name = prof.name.replace(' ', '+');
+      let link = baseURL + name;
+      // Searches for the professor on Rate My Professors
+      // If professor found, then gets the professor's rating
+      scrapeProfLink(link)
+        .then(endLink => {
+          scrapeProfInfo('http://www.ratemyprofessors.com' + endLink)
+            .then(resp => {
+              normalizeMetrics(resp);
+              return resolve(resp);
+            })
+            .catch(e => resolve(e));
+        })
+        .catch(err => resolve(err));
+    } else {
+      // If the cache entry is less than a week old
+      resolve({
+        rating: prof.rating,
+        difficulty: prof.difficulty,
+        again: prof.take_again,
+        sentimentAvg: prof.sentiment
+      });
+    }
+  });
 
   // Normalize metrics in response to be out of 5.0 and fix types
   function normalizeMetrics(res) {
@@ -150,31 +205,7 @@ function getScores(profs, responses, percentages) {
     res.again = res.again ? parseFloat(res.again) / 20 : undefined;
     // Change sentiment scale from -5 to 5 to 0 to 5
     res.sentimentAvg = res.sentimentAvg ? (res.sentimentAvg + 5) / 2 : undefined;
-    console.log(res);
   }
-}
-
-// Base URL for HTTP request
-const baseURL = 'http://www.ratemyprofessors.com/search.jsp?queryoption=HEADER' + 
-  '&queryBy=teacherName&schoolName=University+of+Texas+at+Austin&schoolID=1255&query=';
-
-// Gets the rating for a professor
-function getProfInfo(name) {
-  return new Promise((resolve, reject) => {
-    // Goes through list of professors
-    name = name.replace(' ', '+');
-    // Link to query Rate My Professors
-    let link = baseURL + name;
-    // Searches for the professor on Rate My Professors
-    // If professor found, then gets the professor's rating
-    scrapeProfLink(link)
-      .then(endLink => {
-        scrapeProfInfo('http://www.ratemyprofessors.com' + endLink)
-          .then(rating => resolve(rating))
-          .catch(e => resolve(e));
-      })
-      .catch(err => resolve(err));
-  });
 }
 
 // Scrapes link of professor on Rate My Professors
@@ -188,7 +219,7 @@ function scrapeProfLink(link) {
         let htmas = $('div.not-found-box').next().html();
         if (!htmas.includes('Your search')) {
           // Gets the link for the first result
-          $('.listings').filter(function() {
+          $('.listings').filter(function () {
             let endLink = $($(this)[0].children[3].children[1]).attr('href');
             resolve(endLink);
           });
@@ -234,7 +265,7 @@ function scrapeProfInfo(link) {
         let numComments = 0;
         let sentimentSum = 0;
         let comment, sentimentRes, commentSentiment;
-        $('tr').each(function(i, elem) {
+        $('tr').each(function (i, elem) {
           if (!isNaN($(this).attr('id'))) {
             comment = $(this).children()[2].children[3].children[0].data.trim();
             sentimentRes = sentiment.analyze(comment);
@@ -249,9 +280,9 @@ function scrapeProfInfo(link) {
         response.sentimentAvg = numComments === 0 ? undefined : sentimentSum / numComments;
         // Reject if response has undefined values for each 
         if (!(response.rating ||
-              response.difficulty ||
-              response.again ||
-              response.sentimentAvg)) {
+          response.difficulty ||
+          response.again ||
+          response.sentimentAvg)) {
           reject({});
         }
         resolve(response);
